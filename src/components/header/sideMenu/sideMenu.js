78044ceadd6ea89f36e0900c5bec351558f7a4fa
Arnas Dickus <arnasdickus@protonmail.com>
2020-11-09 17:00:24 +0200

    progress

--- src/components/header/sideMenu/sideMenu.js
+++ src/components/header/sideMenu/sideMenu.js
@@ -1,15 +1,22 @@
 import React from "react";
-import classesHeader from '../header.module.scss';
+import classes from './sideMenu.module.scss';
 import {Link} from "react-router-dom";
 
 const SideMenu = () => {
     return (
         <aside>
-            <div>
-                <Link className={classesHeader.navLink} to="/courses">Kursai</Link>
-                <Link className={classesHeader.navLink} to="/">Akademija</Link>
-                <a className={classesHeader.navLink} href="/#">Prisijungti</a>
-                <a className={classesHeader.navLink} href="/#">Atsijungti </a>
+            <div className={classes.sideMenu}>
+                <div className={classes.card}>
+                    <a className={classes.navLink} href="/#">Prisijungti</a>
+                    <a className={classes.navLink} href="/#">Atsijungti </a>
+                </div>
+
+                <div className={classes.card}>
+                    <Link className={classes.navLink} to="/courses">Kursai</Link>
+                    <Link className={classes.navLink} to="/">Akademija</Link>
+                </div>
+
+
             </div>
         </aside>
     );
