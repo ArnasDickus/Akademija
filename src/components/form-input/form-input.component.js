80754870384b8847ee350eedf3978de966fefc26
Arnas Dickus <arnasdickus@protonmail.com>
2020-11-17 18:14:30 +0200

    remove unused code

--- src/components/form-input/form-input.component.js
+++ src/components/form-input/form-input.component.js
@@ -3,7 +3,7 @@ import classes from './form-input.module.scss'
 
 const FormInput = ({ handleChange, label,  ...otherProps }) => (
     <div className={classes.group}>
-        <input className={classes.formInput} onChange={ handleChange } {...otherProps} ref={register} />
+        <input className={classes.formInput} onChange={ handleChange } {...otherProps} />
         {
             label ? (<label className={`${otherProps.value.length ? `${classes.shrink}` : ''} ${classes.formInputLabel}`}>
                 { label }</label>) : null
