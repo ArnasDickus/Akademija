78ba35aa800e8149e23fce2dcd8247f1a0306e7a
Arnas Dickus <arnasdickus@protonmail.com>
2020-11-17 20:06:31 +0200

    storing progress

--- src/pages/login/login.page.js
+++ src/pages/login/login.page.js
@@ -4,6 +4,9 @@ import FormInput from "../../components/form-input/form-input.component";
 import CustomButton from "../../components/custom-button/custom-button.component";
 import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import { Link } from "react-router-dom";
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import TextInput from "../../components/text-input/text-input.component";
 
 class Login extends React.Component {
     constructor(props) {
@@ -35,6 +38,45 @@ class Login extends React.Component {
     render () {
         return (
             <div className={classes.login}>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        password: '',
+                    }}
+                    validationSchema={Yup.object({
+                        password: Yup.string()
+                            .min(6, 'Must be 6 characters or more')
+                            .required('Required'),
+                        email: Yup.string()
+                            .email('Invalid email address')
+                            .required('Required'),
+                    })}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    <Form>
+                        <TextInput
+                            label="Email Address"
+                            name="email"
+                            type="email"
+                            placeholder="jane@formik.com"
+                        />
+                        <TextInput
+                            label="Password"
+                            name="password"
+                            type="password"
+                            placeholder="Jane"
+                        />
+
+                        <button type="submit">Submit</button>
+                    </Form>
+                </Formik>
+
+
                 <div className={classes.wrapper}>
                     <h2 className={classes.title}>Turi prisijungimą</h2>
                     <span>Prisijunk čia.</span>
