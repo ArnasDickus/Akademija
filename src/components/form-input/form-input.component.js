67f11f09bb9c44562ef0977b8528e7a271c90f43
Arnas Dickus <arnasdickus@protonmail.com>
2020-11-17 18:07:47 +0200

    installed react-hook-form

--- src/components/form-input/form-input.component.js
+++ src/components/form-input/form-input.component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import classes from './form-input.module.scss'
 
-const FormInput = ({ handleChange, label, ...otherProps }) => (
+const FormInput = ({ handleChange, label,  ...otherProps }) => (
     <div className={classes.group}>
-        <input className={classes.formInput} onChange={ handleChange } {...otherProps} />
+        <input className={classes.formInput} onChange={ handleChange } {...otherProps} ref={register} />
         {
             label ? (<label className={`${otherProps.value.length ? `${classes.shrink}` : ''} ${classes.formInputLabel}`}>
                 { label }</label>) : null
