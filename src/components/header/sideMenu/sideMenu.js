d9e9de21e4a6247c0eb4e0efef2bd0a00be82aba
Arnas Dickus <arnasdickus@protonmail.com>
2020-11-24 00:56:31 +0200

    added translation for side menu converted hamburger comp to hooks

--- src/components/header/sideMenu/sideMenu.js
+++ src/components/header/sideMenu/sideMenu.js
@@ -4,27 +4,29 @@ import { Link } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "../../../redux/user/user.selector";
 import { connect } from 'react-redux';
-import {auth} from "../../../firebase/firebase.utils";
-
+import { auth } from "../../../firebase/firebase.utils";
+import { useTranslation } from "react-i18next";
 
 const SideMenu = ({currentUser}) => {
+    const { t } = useTranslation();
+
     return (
         <aside>
             <div className={classes.sideMenu}>
                     {
                         currentUser
                             ?   <div className={classes.card}>
-                                    <div className={classes.navLink} onClick={() => auth.signOut()}>Atsijungti</div>
+                                    <div className={classes.navLink} onClick={() => auth.signOut()}>{t('header.logout')}</div>
                                 </div>
 
                             :   <div className={classes.card}>
-                                    <Link className={classes.navLink} to="/login">Prisijungti</Link>
-                                    <Link className={classes.navLink} to="/register">Registruotis</Link>
+                                    <Link className={classes.navLink} to="/login">{t('header.login')}</Link>
+                                    <Link className={classes.navLink} to="/register">{t('header.register')}</Link>
                                 </div>
                     }
                 <div className={classes.card2}>
-                    <Link className={classes.navLink} to="/courses">Kursai</Link>
-                    <Link className={classes.navLink} to="/">Akademija</Link>
+                    <Link className={classes.navLink} to="/courses">{t('header.courses')}</Link>
+                    <Link className={classes.navLink} to="/">{t('header.academy')}</Link>
                 </div>
             </div>
         </aside>
