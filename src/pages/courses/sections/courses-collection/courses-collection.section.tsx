3b7f4fd8159a659c858d7fb4d441b9b23f048849
Arnas Dickus <arnasdickus@protonmail.com>
2020-11-25 14:19:45 +0200

    added cards for courses

--- src/pages/courses/sections/courses-collection/courses-collection.section.tsx
+++ src/pages/courses/sections/courses-collection/courses-collection.section.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import classes from './courses-collection.module.scss';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 interface Props {
     title: string,
-    lessons: string[]
-
-
+    lessons: string[];
 }
 
 const CourseCollections: React.FC<Props> = ({ title, lessons }) => {
     return (
-        <div>
+        <div className={classes.courseCollections}>
             <h2>{ title }</h2>
-            { lessons.map(title => (
-                <li>{ title }</li>
-            ))}
+            <div className={classes.row}>
+                { lessons.map(title => (
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                { title }
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+                ))}
+            </div>
         </div>
     )
 }
