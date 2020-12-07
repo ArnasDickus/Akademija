9eb329cddc1559b3f639b9d4fbb43f0ce8130720
Arnas Dickus <arnasdickus@protonmail.com>
2020-12-07 16:41:57 +0200

    task finished

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import React, { Suspense } from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 import './i18n';
 
 ReactDOM.render(
-        <Provider store={store}>
-            <BrowserRouter>
-              <React.StrictMode>
-                  <Suspense fallback="">
-                    <App />
-                  </Suspense>
-              </React.StrictMode>
-            </BrowserRouter>
-        </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <Suspense fallback="">
+                    <App/>
+                </Suspense>
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
