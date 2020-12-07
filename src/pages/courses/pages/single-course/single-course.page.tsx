ba772c5079889706401a32eda5ec02c5f8c2cb75
Arnas Dickus <arnasdickus@protonmail.com>
2020-12-07 20:00:40 +0200

    added youtube video course content subject section placeholder

--- src/pages/courses/pages/single-course/single-course.page.tsx
+++ src/pages/courses/pages/single-course/single-course.page.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import YouTube from 'react-youtube';
+import classes from './single-course.module.scss';
+import {CategoriesInterface} from "../../../../core/interfaces/categories.interface";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import SubjectSections from "../../sections/subject-section/subject-section.section";
 
 const SingleCourse: React.FC = () => {
+    const [options,] = useState<any>({
+        width: '1430',
+        height: '647',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 0,
+            'autohide':1,
+            'wmode':'opaque',
+            'origin':'http://localhost:3000' }
+    })
+
+
 
     return (
         <div>
-            <h2>Single course</h2>
+            <div className={classes.row}>
+                <div>
+                    <YouTube videoId="0pThnRneDjw" opts={options} />
+                </div>
+
+                <div className={classes.content}>
+                    <div className={classes.textRow}>
+                        <p className={classes.courseContent}>Course content</p>
+                        <div className={classes.xIcon}>
+                            <FontAwesomeIcon  icon={faTimes}/>
+                        </div>
+                    </div>
+                    <SubjectSections />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/pages/courses/sections/subject-section/subject-section.section.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './subject-section.module.scss';
+
+const SubjectSections = () => {
+    return (
+        <div>
+            Section 1 Introduction
+        </div>
+    )
+}
+
+export default SubjectSections;
