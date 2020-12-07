4a3ae63c6e1507a0a6080383f045c3d9d1ca4002
Arnas Dickus <arnasdickus@protonmail.com>
2020-12-07 20:34:36 +0200

    created subject section added toggle menu

--- src/pages/courses/sections/subject-section/subject-section.section.js
+++ src/pages/courses/sections/subject-section/subject-section.section.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './subject-section.module.scss';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowDown, faArrowUp} from "@fortawesome/free-solid-svg-icons";
 
 const SubjectSections = () => {
+    const [menu, toggleMenu] = useState(false);
+
+    const handleClick = () => {
+        toggleMenu(!menu)
+    }
+
     return (
-        <div>
-            Section 1 Introduction
+        <div onClick={handleClick} className={classes.subjectSections}>
+            <div className={classes.row}>
+                <div>
+                    <h5 className={classes.title}>Section 1 Introduction</h5>
+                    <p className={classes.subtitles}>3/3 | 7min</p>
+                </div>
+
+                <div>
+                    {
+                        menu ?
+                            <FontAwesomeIcon icon={faArrowDown}/>
+                            : <FontAwesomeIcon icon={faArrowUp}/>
+                    }
+                </div>
+
+            </div>
+
+
+
         </div>
     )
 }
