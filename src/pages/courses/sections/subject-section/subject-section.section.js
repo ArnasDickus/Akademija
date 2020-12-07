dffac5de4f87b6a300ae239d230a73cf0cec9e51
Arnas Dickus <arnasdickus@protonmail.com>
2020-12-07 20:47:55 +0200

    stashing changes

--- src/pages/courses/sections/subject-section/subject-section.section.js
+++ src/pages/courses/sections/subject-section/subject-section.section.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import classes from './subject-section.module.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowDown, faArrowUp} from "@fortawesome/free-solid-svg-icons";
+import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox';
 
 const SubjectSections = () => {
     const [menu, toggleMenu] = useState(false);
@@ -11,26 +12,31 @@ const SubjectSections = () => {
     }
 
     return (
-        <div onClick={handleClick} className={classes.subjectSections}>
-            <div className={classes.row}>
-                <div>
-                    <h5 className={classes.title}>Section 1 Introduction</h5>
-                    <p className={classes.subtitles}>3/3 | 7min</p>
-                </div>
+        <React.Fragment>
+            <div onClick={handleClick} className={classes.subjectSections}>
+                <div className={classes.row}>
+                    <div>
+                        <h5 className={classes.title}>Section 1 Introduction</h5>
+                        <p className={classes.subtitles}>3/3 | 7min</p>
+                    </div>
 
-                <div>
-                    {
-                        menu ?
-                            <FontAwesomeIcon icon={faArrowDown}/>
-                            : <FontAwesomeIcon icon={faArrowUp}/>
-                    }
+                    <div>
+                        {
+                            menu ?
+                                <FontAwesomeIcon icon={faArrowDown}/>
+                                : <FontAwesomeIcon icon={faArrowUp}/>
+                        }
+                    </div>
                 </div>
-
             </div>
+            <Checkbox
+                value="checkedA"
+                inputProps={{ 'aria-label': 'Checkbox A' }}
+            />
+            <p>React Concepts</p>
+            <p>2min</p>
+        </React.Fragment>
 
-
-
-        </div>
     )
 }
 
