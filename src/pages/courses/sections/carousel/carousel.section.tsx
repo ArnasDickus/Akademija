a014963c28febc9cd97361d1ec032130b18ced45
Arnas Dickus <arnasdickus@protonmail.com>
2020-12-16 17:45:26 +0200

    created carousel

--- src/pages/courses/sections/carousel/carousel.section.tsx
+++ src/pages/courses/sections/carousel/carousel.section.tsx
@@ -1,13 +1,75 @@
-import React from 'react';
-import classes from './carousel.module.scss';
+import React, { useState } from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: any;
+    value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: any) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 const Carousel: React.FC = () => {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+
     return (
         <div>
-           <h2>Carousel</h2>
-            <h2>Carousel</h2>
-            <h2>Carousel</h2>
-            <h2>Carousel</h2>
+            <Paper>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="simple tabs example"
+                    indicatorColor="primary"
+                    textColor="primary">
+
+                    <Tab label="Search" {...a11yProps(0)} />
+
+                    <Tab label="Overview" {...a11yProps(1)} />
+                    <Tab label="Q&A" {...a11yProps(2)} />
+                </Tabs>
+            </Paper>
+            <TabPanel value={value} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
         </div>
     )
 }
