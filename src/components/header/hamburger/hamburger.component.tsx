9b623f8a13b41cde1d67d5ae8fdbe8ad9ec3c9bc
Arnas Dickus <arnasdickus@protonmail.com>
2021-01-05 07:59:44 +0200

    fix: 🐛 minor navbar fixes
    fixed header alignment, and onclick sidemenu closes
    
    ✅ Closes: #33

--- src/components/header/hamburger/hamburger.component.tsx
+++ src/components/header/hamburger/hamburger.component.tsx
@@ -1,33 +1,34 @@
-import React, {useState} from "react";
-import {faTimes, faBars} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {useTranslation} from "react-i18next";
+import React from 'react';
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTranslation } from 'react-i18next';
 
-import classes from "./hamburger.module.scss";
+import classes from './hamburger.module.scss';
 
 type Props = {
-    menuOpen: () => void;
-}
+  menuOpen: () => void;
+  hamburgerOpen: () => void;
+  hamburger: boolean;
+};
 
 const Hamburger: React.FC<Props> = (props) => {
-    const [hamburger, openHamburger] = useState(false);
-    const {t} = useTranslation();
+  const { t } = useTranslation();
 
-    const handleClick = (): void => {
-        openHamburger(!hamburger)
-        props.menuOpen();
-    }
+  const handleClick = (): void => {
+    props.hamburgerOpen();
+    props.menuOpen();
+  };
 
-    return (
-        <div className={classes.hamburger}>
-            <span className={classes.title}>{t('header.academy')}</span>
-            {
-                hamburger ?
-                    <FontAwesomeIcon icon={faTimes} onClick={handleClick}/>
-                    : <FontAwesomeIcon icon={faBars} onClick={handleClick}/>
-            }
-        </div>
-    )
-}
+  return (
+    <div className={classes.hamburger}>
+      <span className={classes.title}>{t('header.academy')}</span>
+      {props.hamburger ? (
+        <FontAwesomeIcon icon={faTimes} onClick={handleClick} />
+      ) : (
+        <FontAwesomeIcon icon={faBars} onClick={handleClick} />
+      )}
+    </div>
+  );
+};
 
 export default Hamburger;
