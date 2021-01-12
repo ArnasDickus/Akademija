b21ce547931349b892a2eb9803f7248a4a898a41
Arnas Dickus <arnasdickus@protonmail.com>
2021-01-12 06:11:16 +0200

    feat: 🎸 added test list items
    added practice list items
    
    ✅ Closes: #38

--- src/core/types/categories.types.tsx
+++ src/core/types/categories.types.tsx
@@ -1,30 +1,36 @@
 export type CategoriesType = {
-    id: string;
-    title: string;
-    url: string;
-    courses: CoursesType[];
-}
+  id: string;
+  title: string;
+  url: string;
+  courses: CoursesType[];
+};
 
 export type CoursesType = {
-    id?: string;
-    title: string;
-    about: string;
-    description: string;
-    img: string;
-    url: string;
-    sections: CourseSectionType[];
-}
+  id?: string;
+  title: string;
+  about: string;
+  description: string;
+  img: string;
+  url: string;
+  sections: CourseSectionType[];
+};
 
 export type CourseSectionType = {
-    id: string;
-    title: string;
-    oldId?: string;
-    lessons: LessonsType[],
-}
+  id: string;
+  title: string;
+  oldId?: string;
+  lessons: LessonsType[];
+  tests?: TestsType[];
+};
 
 export type LessonsType = {
-    id?: string;
-    title: string;
-    url: string;
-}
+  id?: string;
+  title: string;
+  url: string;
+};
 
+export type TestsType = {
+  id: string;
+  title: string;
+  url: string;
+};
--- src/pages/courses/sections/subject-section/subject-section.section.tsx
+++ src/pages/courses/sections/subject-section/subject-section.section.tsx
@@ -1,72 +1,79 @@
-import React, {useState} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowDown, faArrowUp} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import Checkbox from '@material-ui/core/Checkbox';
-import {CourseSectionType} from "core/types/categories.types";
+import { CourseSectionType } from 'core/types/categories.types';
 
 import classes from './subject-section.module.scss';
 
-type Props = CourseSectionType & {onUrlUpdate: (url: string, oldId: string) => void};
+type Props = CourseSectionType & { onUrlUpdate: (url: string, oldId: string) => void };
 
-const SubjectSections: React.FC<Props> = ({
-                                                               id,
-                                                               title,
-                                                               lessons,
-                                                               onUrlUpdate,
-                                                               oldId}) => {
-    const [menu, toggleMenu] = useState(false);
-    const [lessonId, setLessonId] = useState('');
+const SubjectSections: React.FC<Props> = ({ id, title, lessons, tests, onUrlUpdate, oldId }) => {
+  const [menu, toggleMenu] = useState(false);
+  const [lessonId, setLessonId] = useState('');
 
-    const handleClick = () => {
-        toggleMenu(!menu)
-    }
+  const handleClick = () => {
+    toggleMenu(!menu);
+  };
 
+  const handleLessonClick = (lessonUrl: string, lessonId: string, currentSectionId: string) => {
+    onUrlUpdate(lessonUrl, currentSectionId);
+    setLessonId(lessonId);
+  };
 
-    const handleLessonClick = (lessonUrl: string, lessonId: string, currentSectionId: string) => {
-        onUrlUpdate(lessonUrl, currentSectionId)
-        setLessonId(lessonId);
-    }
+  return (
+    <React.Fragment>
+      <div className={classes.subjectSections} onClick={handleClick}>
+        <div className={classes.row}>
+          <div>
+            <h5 className={classes.title}>{title}</h5>
+            <p className={classes.subtitles}>3/3 | 7min</p>
+          </div>
 
-    return (
-        <React.Fragment>
-            <div className={classes.subjectSections} onClick={handleClick}>
-                <div className={classes.row}>
-                    <div>
-                        <h5 className={classes.title}>{title}</h5>
-                        <p className={classes.subtitles}>3/3 | 7min</p>
-                    </div>
+          <div>
+            {menu ? <FontAwesomeIcon icon={faArrowDown} /> : <FontAwesomeIcon icon={faArrowUp} />}
+          </div>
+        </div>
+      </div>
 
-                    <div>
-                        {
-                            menu ?
-                                <FontAwesomeIcon icon={faArrowDown}/>
-                                : <FontAwesomeIcon icon={faArrowUp}/>
-                        }
-                    </div>
-                </div>
+      {menu
+        ? lessons.map((lesson) => (
+            <div
+              key={lesson.id}
+              className={`${classes.dropdown} ${
+                lesson.id === lessonId && oldId === id ? `${classes.activeLesson}` : ''
+              }`}
+              onClick={() => handleLessonClick(lesson.url, lesson.id || '', id || '')}
+            >
+              <div className={classes.rowDropdown}>
+                <Checkbox inputProps={{ 'aria-label': 'Checkbox A' }} value="checkedA" />
+                <span>{lesson.title}</span>
+              </div>
+              <span className={classes.time}>2 min</span>
             </div>
+          ))
+        : ''}
+      {menu ? <h5 className={classes.practiceTitle}>Practice</h5> : ''}
 
-            {
-                menu ?
-                    (
-                        lessons.map((lesson) => (
-                            <div key={lesson.id} className={`${classes.dropdown} ${lesson.id === lessonId && oldId === id
-                                ? `${classes.activeLesson}` : ''}`}
-                                 onClick={() => handleLessonClick(lesson.url, lesson.id || '', id || '')}>
-                                <div className={classes.rowDropdown}>
-                                    <Checkbox
-                                        inputProps={{'aria-label': 'Checkbox A'}}
-                                        value="checkedA"
-                                    />
-                                    <span>{lesson.title}</span>
-                                </div>
-                                <span className={classes.time}>2 min</span>
-                            </div>
-                        ))
-                    ) : ''
-            }
-        </React.Fragment>
-    )
-}
+      {menu
+        ? tests?.map((test) => (
+            <div
+              key={test.id}
+              className={`${classes.dropdown} ${
+                test.id === lessonId && oldId === id ? `${classes.activeLesson}` : ''
+              }`}
+              onClick={() => handleLessonClick(test.url, test.id || '', id || '')}
+            >
+              <div className={classes.rowDropdown}>
+                <Checkbox inputProps={{ 'aria-label': 'Checkbox A' }} value="checkedA" />
+                <span>{test.title}</span>
+              </div>
+              <span className={classes.time}>2 min</span>
+            </div>
+          ))
+        : ''}
+    </React.Fragment>
+  );
+};
 
 export default SubjectSections;
