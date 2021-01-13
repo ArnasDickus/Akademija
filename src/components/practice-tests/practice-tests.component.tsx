1976603bba2073b4ebe440569543f7e8d92a8f41
Arnas Dickus <arnasdickus@protonmail.com>
2021-01-13 07:42:58 +0200

    feat: 🎸 finished building testpage
    ✅ Closes: #38

--- src/components/practice-tests/practice-tests.component.tsx
+++ src/components/practice-tests/practice-tests.component.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-// import classes from './practice-tests.module.scss';
+import wrapper from 'baseScss/components/wrapper.module.scss';
+
+import classes from './practice-tests.module.scss';
 
 type Props = {
   testData: {
@@ -12,11 +14,19 @@ type Props = {
 
 const PracticeTests: React.FC<Props> = ({ testData }) => {
   return (
-    <div>
-      <p>id: {testData.id}</p>
-      <p>title: {testData.title}</p>
-      <p>description: {testData.description}</p>
-      <p>question: {testData.question}</p>
+    <div className={classes.container}>
+      <h1 className={classes.title}>{testData.title}</h1>
+      <div className={wrapper.testWrapper}>
+        <p>description: {testData.description}</p>
+        <p>question: {testData.question}</p>
+        <div className={classes.row}>
+          <span>
+            Stuck?
+            <button className={classes.videoButton}>Watch a video or use a hint</button>
+          </span>
+          <button className={classes.problemButton}>Report a problem</button>
+        </div>
+      </div>
     </div>
   );
 };
