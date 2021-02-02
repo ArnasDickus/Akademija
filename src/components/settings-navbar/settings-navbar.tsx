fedd178ec01875184752c2202db251f6a61eda1e
Arnas Dickus <arnasdickus@protonmail.com>
2021-02-02 07:18:20 +0200

    feat: 🎸 created settings navbar stylized it

--- src/components/settings-navbar/settings-navbar.tsx
+++ src/components/settings-navbar/settings-navbar.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import AllRoutesEnum from 'core/enums/allRoutes.enum';
+
+import classes from './settings-navbar.module.scss';
 
 const SettingsNavbar: React.FC = () => {
   return (
-    <div>
-      <p>Settings Navbar</p>
+    <div className={classes.row}>
+      <div className={classes.avatarContainer}>
+        <div className={classes.avatar} />
+        <p className={classes.accountName}>Avatar Name</p>
+      </div>
+      <div>
+        <ul className={classes.ul}>
+          <li className={classes.listItem}>
+            <Link className={classes.link} to={`/${AllRoutesEnum.SETTINGS_PROFILE}`}>
+              Profile
+            </Link>
+          </li>
+
+          <li className={classes.listItem}>
+            <Link className={classes.link} to={`/${AllRoutesEnum.SETTINGS_PHOTO}`}>
+              Photo
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/settings/settings-template.tsx
+++ src/pages/settings/settings-template.tsx
@@ -4,6 +4,9 @@ import SettingsProfile from 'pages/settings/pages/settings-profile/settings-prof
 import SettingsPhoto from 'pages/settings/pages/settings-photo/settings-photo';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AllRoutesEnum from 'core/enums/allRoutes.enum';
+import wrapper from 'baseScss/components/wrapper.module.scss';
+
+import classes from './settings-template.module.scss';
 
 const SettingsTemplate: React.FC = () => {
   //  const history = useHistory();
@@ -15,19 +18,27 @@ const SettingsTemplate: React.FC = () => {
   //   });
 
   return (
-    <div>
-      <SettingsNavbar />
-      <p>Settings Template</p>
-      <Switch>
-        <Router>
-          <Route path={AllRoutesEnum.SETTINGS_PROFILE}>
-            <SettingsProfile />
-          </Route>
-          <Route path={AllRoutesEnum.SETTINGS_PHOTO}>
-            <SettingsPhoto />
-          </Route>
-        </Router>
-      </Switch>
+    <div className={classes.container}>
+      <div className={wrapper.headerWrapper}>
+        <div className={classes.row}>
+          <div>
+            <SettingsNavbar />
+          </div>
+          <div>
+            <p>Settings Template</p>
+            <Switch>
+              <Router>
+                <Route path={AllRoutesEnum.SETTINGS_PROFILE}>
+                  <SettingsProfile />
+                </Route>
+                <Route path={AllRoutesEnum.SETTINGS_PHOTO}>
+                  <SettingsPhoto />
+                </Route>
+              </Router>
+            </Switch>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
