b8ce5e45eb6381eb4dac760fb1b172cd5860700e
Arnas Dickus <arnasdickus@protonmail.com>
2021-02-09 07:40:46 +0200

    feat: 🎸 finished profile image
    ✅ Closes: #53

--- src/pages/settings/pages/settings-photo/settings-photo.tsx
+++ src/pages/settings/pages/settings-photo/settings-photo.tsx
@@ -1,73 +1,27 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState } from 'react';
 import wrapper from 'baseScss/components/wrapper.module.scss';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
-import ReactCrop, { Crop } from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
+import SuccessComponent from 'components/success-message/success-message.component';
+import CustomButton from 'components/custom-button/custom-button.component';
+import { useTranslation } from 'react-i18next';
 
 import classes from './settings-photo.module.scss';
 
 const SettingsPhoto: React.FC = () => {
-  const [upImg, setUpImg] = useState('');
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const imgRef = useRef<any>(null);
-  const [crop, setCrop] = useState<Crop>({ unit: '%', width: 75, height: 100 });
+  const { t } = useTranslation();
   const [displayAccountIcon, setDisplayAccountIcon] = useState(true);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const previewCanvasRef = useRef<any>(null);
-  const [completedCrop, setCompletedCrop] = useState<Crop>();
 
   const onSelectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       setDisplayAccountIcon(false);
-      const reader = new FileReader();
-      reader.addEventListener('load', () => setUpImg(reader.result as string));
-      reader.readAsDataURL(event.target.files[0]);
     }
   };
 
-  const onLoad = useCallback((img) => {
-    imgRef.current = img;
-  }, []);
-
-  useEffect(() => {
-    if (!completedCrop || !previewCanvasRef.current || !imgRef.current) {
-      return;
-    }
-
-    const image = imgRef.current;
-    const canvas = previewCanvasRef.current;
-    const crop = completedCrop;
-    const scaleX = image.naturalWidth / image.width;
-    const scaleY = image.naturalHeight / image.height;
-    const ctx = canvas.getContext('2d');
-    const pixelRatio = window.devicePixelRatio;
-
-    if (
-      typeof crop.width !== 'undefined' &&
-      typeof crop.height !== 'undefined' &&
-      typeof crop.x !== 'undefined' &&
-      typeof crop.y !== 'undefined'
-    ) {
-      canvas.width = crop.width * pixelRatio;
-      canvas.height = crop.height * pixelRatio;
-
-      ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-      ctx.imageSmoothingQuality = 'high';
-
-      ctx.drawImage(
-        image,
-        crop.x * scaleX,
-        crop.y * scaleY,
-        crop.width * scaleX,
-        crop.height * scaleY,
-        0,
-        0,
-        crop.width,
-        crop.height,
-      );
-    }
-  }, [completedCrop]);
+  const handleUpload = (event: React.MouseEvent<Element, MouseEvent>) => {
+    event.preventDefault();
+  };
 
   return (
     <div className={classes.container}>
@@ -78,32 +32,19 @@ const SettingsPhoto: React.FC = () => {
             {displayAccountIcon ? (
               <AccountCircleIcon className={classes.icon} />
             ) : (
-              <ReactCrop
-                className={classes.image}
-                crop={crop}
-                src={upImg}
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                onChange={(event: any) => setCrop(event)}
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                onComplete={(event: any) => setCompletedCrop(event)}
-                onImageLoaded={onLoad}
-              />
+              <p>Image file</p>
             )}
           </div>
-          {/* <canvas
-            ref={previewCanvasRef}
-            // Rounding is important so the canvas width and height matches/is a multiple for sharpness.
-            style={{
-              width: Math.round(completedCrop?.width ?? 0),
-              height: Math.round(completedCrop?.height ?? 0),
-            }}
-          /> */}
         </div>
         <p>Add / Change image:</p>
         <Button component="label" variant="contained">
           Upload File
           <input type="file" hidden onChange={(event) => onSelectFile(event)} />
         </Button>
+        <div onClick={(event) => handleUpload(event)}>
+          <CustomButton type="submit"> {t('profile.saveChanges')} </CustomButton>
+        </div>
+        <SuccessComponent>{t('profile.successMessage')}</SuccessComponent>
       </div>
     </div>
   );
