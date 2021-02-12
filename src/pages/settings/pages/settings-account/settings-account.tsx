a9dd9f1d82d4b0214d7a3429096b4b37a78b7ab0
Arnas Dickus <arnasdickus@protonmail.com>
2021-02-12 07:38:01 +0200

    feat: 🎸 stashing changes

--- src/pages/settings/pages/settings-account/settings-account.tsx
+++ src/pages/settings/pages/settings-account/settings-account.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import wrapper from 'baseScss/components/wrapper.module.scss';
+
+import classes from './settings-account.module.scss';
 
 const SettingsAccount: React.FC = () => {
   return (
-    <div>
-      <p>Settings Account</p>
+    <div className={classes.container}>
+      <div className={wrapper.profileSettingsWrapper}>
+        <h2 className={classes.title}>Email Address</h2>
+        <p className={classes.subtitle}>Add or remove email adresses on your accont</p>
+        <p>Email addresses you added:</p>
+        <div className={classes.row}>
+          <p>Email address</p>
+          <p>Primary</p>
+        </div>
+
+        <p>Add Email Adress</p>
+      </div>
     </div>
   );
 };
