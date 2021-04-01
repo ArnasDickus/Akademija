c889f310e472a12ea5431a3194d3b1fa6ac15c4b
Arnas Dickus <arnasdickus@protonmail.com>
2021-04-01 07:10:26 +0300

    refactor: 💡 refactor register page

--- src/components/lang/register/register.page.tsx
+++ src/components/lang/register/register.page.tsx
@@ -1,4 +1,3 @@
-import wrapper from 'baseScss/components/wrapper.module.scss';
 import CustomButton from 'components/shared/custom-button/custom-button.component';
 import ErrorComponent from 'components/shared/error-message/error-message.component';
 import FormInput from 'components/shared/form-input/form-input.component';
@@ -6,19 +5,20 @@ import { auth, createUserProfileDocument, sendEmailVerification } from 'firebase
 import { Form, Formik } from 'formik';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { AuthWrapper } from 'styles/components/wrapper';
 import * as Yup from 'yup';
 
-import classes from './register.module.scss';
+import * as S from './register.styled';
 
 const Register: React.FC = () => {
   const [errorType, setErrorType] = useState('');
   const [hasError, setHasError] = useState(false);
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return (
-    <div className={classes.register}>
-      <div className={wrapper.authWrapper}>
-        <h2 className={classes.title}>{t('login.cantLogin')} </h2>
+    <S.Container>
+      <AuthWrapper>
+        <h2 className="title">{t('login.cantLogin')} </h2>
         <span>{t('login.register')}</span>
 
         <Formik
@@ -29,16 +29,16 @@ const Register: React.FC = () => {
             confirmPassword: '',
           }}
           validationSchema={Yup.object({
-            displayName: Yup.string().required(i18n.t('errorMessage.required')),
+            displayName: Yup.string().required(t('errorMessage.required')),
             email: Yup.string()
-              .email(i18n.t('errorMessage.invalidEmail'))
-              .required(i18n.t('errorMessage.required')),
+              .email(t('errorMessage.invalidEmail'))
+              .required(t('errorMessage.required')),
             password: Yup.string()
-              .min(6, i18n.t('errorMessage.passwordMin'))
-              .required(i18n.t('errorMessage.required')),
+              .min(6, t('errorMessage.passwordMin'))
+              .required(t('errorMessage.required')),
             confirmPassword: Yup.string()
-              .oneOf([Yup.ref('password'), undefined], i18n.t('errorMessage.passwordsNoMatch'))
-              .required(i18n.t('errorMessage.required')),
+              .oneOf([Yup.ref('password'), undefined], t('errorMessage.passwordsNoMatch'))
+              .required(t('errorMessage.required')),
           })}
           onSubmit={async (values) => {
             const { displayName, email, password } = values;
@@ -71,8 +71,8 @@ const Register: React.FC = () => {
             <CustomButton type="submit">{t('login.register')}</CustomButton>
           </Form>
         </Formik>
-      </div>
-    </div>
+      </AuthWrapper>
+    </S.Container>
   );
 };
 
